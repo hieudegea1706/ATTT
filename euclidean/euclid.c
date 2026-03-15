37272ab10741c5ebc514a13df8f8642453d50900
hieudegea1706 <trunghieua1k59@gmail.com>
2026-03-15 18:10:15 +0700

    Add euclidean algorithm in GF10

---
+++ euclidean/euclid.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+#define MOD 0x409   // x^10 + x^3 + 1 = 10000001001 = 1033
+
+// tìm bậc đa thức
+int degree(int a)
+{
+    int deg = -1;
+    while (a)
+    {
+        a >>= 1;
+        deg++;
+    }
+    return deg;
+}
+
+// phép chia đa thức trong GF(2)
+int poly_div(int a, int b, int *rem)
+{
+    int da = degree(a);
+    int db = degree(b);
+
+    int q = 0;
+
+    while (da >= db)
+    {
+        int shift = da - db;
+        q ^= (1 << shift);
+        a ^= (b << shift);
+
+        da = degree(a);
+    }
+
+    *rem = a;
+    return q;
+}
+
+// Euclidean mở rộng
+int inverse(int a)
+{
+    int r0 = MOD;
+    int r1 = a;
+
+    int t0 = 0;
+    int t1 = 1;
+
+    printf("\n===== START =====\n");
+
+    while (r1 != 0)
+    {
+        int r;
+        int q = poly_div(r0, r1, &r);
+
+        printf("r0 = %d, r1 = %d\n", r0, r1);
+        printf("q = %d\n", q);
+        printf("r = %d\n\n", r);
+
+        int temp = r0;
+        r0 = r1;
+        r1 = r;
+
+        int t = t0 ^ (q * t1);
+
+        t0 = t1;
+        t1 = t;
+    }
+
+    printf("GCD = %d\n", r0);
+
+    if (r0 != 1)
+    {
+        printf("No inverse\n");
+        return -1;
+    }
+
+    return t0;
+}
+
+int main()
+{
+    int a = 523;
+    int b = 1015;
+
+    printf("Inverse of a = %d\n", inverse(a));
+    printf("\n\n");
+
+    printf("Inverse of b = %d\n", inverse(b));
+
+    return 0;
+}
