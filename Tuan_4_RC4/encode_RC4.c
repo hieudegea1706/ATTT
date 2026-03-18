7a79b41039da6b8d4dfb311f8581a65da2e8e06a
hieudegea1706 <trunghieua1k59@gmail.com>
2026-03-18 22:51:06 +0700

    Debug and Run and Test

--- Tuan_4_RC4/encode_RC4.c
+++ Tuan_4_RC4/encode_RC4.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-// Hàm hoán vị 2 phần tử
+
 void swap(unsigned char *a, unsigned char *b) {
     unsigned char temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// Giai đoạn 1: Lập lịch khóa (Key-Scheduling Algorithm)
+
 void rc4_ksa(unsigned char *S, const unsigned char *key, int key_len) {
     int i, j = 0;
     
-    // Khởi tạo mảng S từ 0 đến 255
     for (i = 0; i < 256; i++) {
         S[i] = i;
     }
     
-    // Xáo trộn mảng S dựa trên Key
+    
     for (i = 0; i < 256; i++) {
         j = (j + S[i] + key[i % key_len]) % 256;
         swap(&S[i], &S[j]);
     }
 }
-// Giai đoạn 2: Trình tạo giả ngẫu nhiên & Mã hóa (PRGA)
+
 void rc4_prga(unsigned char *S, const unsigned char *plaintext, unsigned char *ciphertext, int text_len) {
     int i = 0, j = 0, k;
     
@@ -35,13 +34,13 @@ void rc4_prga(unsigned char *S, const unsigned char *plaintext, unsigned char *c
         int t = (S[i] + S[j]) % 256;
         unsigned char keystream_byte = S[t];
         
-        // Thực hiện mã hóa: Ciphertext = Plaintext XOR Keystream
+        
         ciphertext[k] = plaintext[k] ^ keystream_byte;
     }
 }
 
 int main() {
-    // 1. Chuẩn bị dữ liệu
+    
     unsigned char key[] = "SecretKey123"; 
     unsigned char plaintext[] = "cybersecurity";
     int text_len = strlen((char *)plaintext);
@@ -51,7 +50,7 @@ int main() {
     unsigned char ciphertext[256];
     unsigned char decryptedtext[256];
 
-    // 2. Quá trình Mã hóa
+    
     rc4_ksa(S, key, key_len);
     rc4_prga(S, plaintext, ciphertext, text_len);
 
@@ -62,11 +61,10 @@ int main() {
     }
     printf("\n");
 
-    // 3. Quá trình Giải mã
-    // QUAN TRỌNG: Phải thiết lập lại (reset) mảng S bằng hàm KSA trước khi giải mã
+    
     rc4_ksa(S, key, key_len);
     rc4_prga(S, ciphertext, decryptedtext, text_len);
-    decryptedtext[text_len] = '\0'; // Thêm ký tự kết thúc chuỗi
+    decryptedtext[text_len] = '\0'; 
 
     printf("Decrypted text: %s\n", decryptedtext);
 
